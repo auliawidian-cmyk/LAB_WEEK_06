5815fe82cd3305c19dad0bf85043930b5a8ec451
ul.auliaaa <163988764+auliaaaaidr@users.noreply.github.com>
2025-10-01 20:47:34 +0700

    ASSIGNMENT

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -41,27 +41,16 @@ class MainActivity : AppCompatActivity() {
         // Tambahkan data dummy
         catAdapter.setData(
             listOf(
-                CatModel(
-                    Gender.Male,
-                    CatBreed.BalineseJavanese,
-                    "Fred",
-                    "Silent and deadly",
-                    "https://cdn2.thecatapi.com/images/7dj.jpg"
-                ),
-                CatModel(
-                    Gender.Female,
-                    CatBreed.ExoticShorthair,
-                    "Wilma",
-                    "Cuddly assassin",
-                    "https://cdn2.thecatapi.com/images/egv.jpg"
-                ),
-                CatModel(
-                    Gender.Unknown,
-                    CatBreed.AmericanCurl,
-                    "Curious George",
-                    "Award winning investigator",
-                    "https://cdn2.thecatapi.com/images/bar.jpg"
-                )
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Iqbaal", "Silent and deadly", "https://cdn2.thecatapi.com/images/7dj.jpg"),
+                CatModel(Gender.Female, CatBreed.ExoticShorthair, "Aulia", "Cuddly assassin", "https://cdn2.thecatapi.com/images/egv.jpg"),
+                CatModel(Gender.Male, CatBreed.AmericanCurl, "Cakwee", "Award winning investigator", "https://cdn2.thecatapi.com/images/bar.jpg"),
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Alip", "Loves sunbeams", "https://cdn2.thecatapi.com/images/1.jpg"),
+                CatModel(Gender.Female, CatBreed.ExoticShorthair, "Helehele", "Fast asleep", "https://cdn2.thecatapi.com/images/2.jpg"),
+                CatModel(Gender.Unknown, CatBreed.AmericanCurl, "Nala", "Queen of the sofa", "https://cdn2.thecatapi.com/images/3.jpg"),
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Ollie", "Chases laser dots", "https://cdn2.thecatapi.com/images/4.jpg"),
+                CatModel(Gender.Unknown, CatBreed.ExoticShorthair, "Zelda", "Curious about boxes", "https://cdn2.thecatapi.com/images/5.jpg"),
+                CatModel(Gender.Female, CatBreed.AmericanCurl, "Poppy", "Snack enthusiast", "https://cdn2.thecatapi.com/images/6.jpg"),
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Simba", "Really loud purr", "https://cdn2.thecatapi.com/images/7.jpg")
             )
         )
     }
